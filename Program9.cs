0f105f0466e5eb0c8c6a687a8fbd8b42ec4cdbe7
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 21:58:32 +0530

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program9
+{
+    static void Main()
+    {
+        string file = "student.txt";
+
+        File.WriteAllText(file,
+            "Name: Kirti\n" +
+            "Roll No: 101\n" +
+            "Course: MCA\n" +
+            "Marks: 85");
+        
+        string data = File.ReadAllText(file);
+
+        Console.WriteLine("Student Details:");
+        Console.WriteLine(data);
+    }
+}
