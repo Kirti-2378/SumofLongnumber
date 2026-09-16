990dfe41aee8dde16a2b981f573a066c85798724
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 09:11:12 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program2 
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "ABC";
+                int b = int.Parse(str);
+                int a = 10;
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format");
+            }
+        }
+    }
+}
