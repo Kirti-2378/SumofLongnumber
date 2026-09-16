7d1da84b758dc95bfad0a54d5302e4afc19b7f1d
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 09:40:32 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program4
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first number: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int result = a / b;
+                Console.WriteLine("Result: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Error: Format Exception.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+        }
+    }
+}
