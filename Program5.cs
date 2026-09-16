f5bd08ae723f2ba86b4392ea6759b3e0339c158b
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 09:02:20 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program5
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+        }
+    }
+}
