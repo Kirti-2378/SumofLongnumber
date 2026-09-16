31d36914b384c819c9330fd62f58670e473e2688
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 09:23:39 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = { 15, 12, 36, 45, 78 };
+                Console.WriteLine(arr[6]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range");
+            }
+        }
+    }
+}
