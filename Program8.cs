b3770dd24dc90f255383fd73236905f62bb1e735
Kirti Kumar <kirtikumar237823@gmail.com>
2026-09-16 21:55:46 +0530

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException(string message) : base(message)
+    {
+    }
+}
+
+class BankAccount
+{
+    double balance;
+
+    public BankAccount(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            throw new InsufficientBalanceException(
+                "Insufficient balance for withdrawal."
+            );
+        }
+
+        balance = balance - amount;
+        Console.WriteLine("Withdrawal successful.");
+        Console.WriteLine("Remaining Balance: " + balance);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount(5000);
+
+        try
+        {
+            Console.Write("Enter withdrawal amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            account.Withdraw(amount);
+        }
+        catch (InsufficientBalanceException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Transaction completed.");
+        }
+    }
+}
+
